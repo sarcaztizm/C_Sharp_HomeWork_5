a711b7666a6e3cb1487cf68bb15556701f0f7414
Eric <ericismag@outlook.com>
2022-09-15 14:48:35 +0300

    First Program

---
+++ EX1_CountEven/Program.cs
@@ -0,0 +1,42 @@
+﻿// Массив заполенный случайными трехзначными числами.
+// Показать количество четных чисел в массив
+
+Console.Clear();
+
+Console.WriteLine("Введите размерность массива");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int [] MyArray = new int[N];
+
+void ArrayInput (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+}
+
+void PrintArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}  ");
+    }
+    Console.WriteLine();
+}
+
+int CountArray(int [] array)
+{
+    int Count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        Count++;
+    }
+    return Count;
+}
+
+ArrayInput (MyArray);
+Console.WriteLine($"Массив на {N} элементов:");
+PrintArray (MyArray);
+Console.WriteLine("Количество четных чисел в массиве: " + CountArray(MyArray));
